c8a4cdedbf725ac2ccc1fc206f5cd01c1628f1d3
Bryan <bryan.valarezo24@gmail.com>
2019-09-21 10:48:41 -0400

    fixing the links

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -2,16 +2,16 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index')
 def index():
 	user = {'username': 'Judy'}
 	return render_template('index.html', title='Home', user=user)
 	
-@app.route('/camera.html')
+@app.route('/camera')
 def camera():
 	return render_template('camera.html', title='Camera')
 
-@app.route('/gallery.html')
+@app.route('/gallery')
 def gallery():
 	return render_template('gallery.html', title='Gallery')
 
