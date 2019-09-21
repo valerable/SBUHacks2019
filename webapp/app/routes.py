b38410a07d035101e5b862b026807847c94a368d
Manny <Immanuel.Almosara@stonybrook.edu>
2019-09-21 12:50:06 -0400

    Bryan work

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import Flask, render_template, make_response
 from flask import redirect, request, jsonify, url_for
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html', title='Home')
-	
+
 @app.route('/camera', methods = ['GET'])
 def camera():
 	return render_template('camera.html', title='Camera')
@@ -18,3 +19,10 @@ def gallery():
 @app.route("/upload", methods=['POST'])
 def upload():
 	return send_from_directory('/myimages','test.jpeg')
+
+def send_to_img_processor(img):
+	dir = os.path.dirname(__file__)
+	img_processor_path = dir + '/../../Neural-Style-Transfer/Network.py'
+	final_img_path = dir + '/../../Neural-Style-Transfer/Network.py'
+
+	os.system()
