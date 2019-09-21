252b22167acd66560b16d2ee7970d1b376923ab6
Manny <Immanuel.Almosara@stonybrook.edu>
2019-09-21 13:25:47 -0400

    bruh2

--- webapp/app/static/scripts/camera.js
+++ webapp/app/static/scripts/camera.js
@@ -24,6 +24,32 @@ function startWebcam() {
 //---------------------
 var canvas, ctx;
 
+function dataURItoBlob(dataURI) {
+	// convert base64 to raw binary data held in a string
+	// doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+	var byteString = atob(dataURI.split(',')[1]);
+
+	// separate out the mime component
+	var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+	// write the bytes of the string to an ArrayBuffer
+	var ab = new ArrayBuffer(byteString.length);
+	var ia = new Uint8Array(ab);
+	for (var i = 0; i < byteString.length; i++) {
+		ia[i] = byteString.charCodeAt(i);
+	}
+
+	//Old Code
+	//write the ArrayBuffer to a blob, and you're done
+	//var bb = new BlobBuilder();
+	//bb.append(ab);
+	//return bb.getBlob(mimeString);
+
+	//New Code
+	return new Blob([ab], {type: mimeString});
+
+
+}
 function init() {
 	video = document.querySelector('video');
 	// Get the canvas and obtain a context for
@@ -39,5 +65,12 @@ function snapshot() {
 	document.querySelector('#dl-btn').href = dataURL;
 	console.log(dataURL)
 	//NOW WE NEED TO SEND IT TO FLASK
-
+	let blob = dataURItoBlob(dataURL);
+	var fd = new FormData(document.forms[0]);
+	var xhr = new XMLHttpRequest();
+	fd.append("myFile", blob);
+	xhr.open('POST', '/', true);
+	xhr.send(fd);
 }
+
+
