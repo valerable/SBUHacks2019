e1ca669e34c24dee4e8de319e9b81a5c27d70559
Manny <Immanuel.Almosara@stonybrook.edu>
2019-09-21 08:53:31 -0400

    Got camera embedded on webpage

--- webapp/app/templates/scripts/script.js
+++ webapp/app/templates/scripts/script.js
@@ -1,25 +1,38 @@
 const captureVideoButton =
-  document.querySelector('#screenshot .capture-button');
+document.querySelector('#capture-button');
 const screenshotButton = document.querySelector('#screenshot-button');
-const img = document.querySelector('#screenshot img');
-const video = document.querySelector('#screenshot video');
-
+const img = document.querySelector('img');
+const video = document.querySelector('video');
 const canvas = document.createElement('canvas');
-
+let context = canvas.getContext('2d');
 captureVideoButton.onclick = function() {
-  navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
+	navigator.mediaDevices.getUserMedia(constraints).
+		then(handleSuccess).catch(handleError);
 };
 
 screenshotButton.onclick = video.onclick = function() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0);
-  // Other browsers will fall back to image/png
-  img.src = canvas.toDataURL('image/webp');
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	canvas.getContext('2d').drawImage(video, 0, 0);
+	// Other browsers will fall back to image/png
+	img.src = canvas.toDataURL('image/webp');
 };
 
 function handleSuccess(stream) {
-  screenshotButton.disabled = false;
-  video.srcObject = stream;
+	screenshotButton.disabled = false;
+	video.srcObject = stream;
 }
+
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+// Trigger photo take
+screenshotButton.addEventListener("click", function() {
+	context.drawImage(video, 0, 0, 640, 480);
+});
