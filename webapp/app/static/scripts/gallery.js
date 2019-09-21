001a57e8b1b1070a6f244172e736c31822c28ac2
judy0501 <judychen92174@gmail.com>
2019-09-21 14:32:31 -0400

    gallery

--- webapp/app/static/scripts/gallery.js
+++ webapp/app/static/scripts/gallery.js
@@ -2,7 +2,7 @@ function getImages(n) {
 	for (var i = 0; i < n; i++){
 		var image = document.createElement("img");
 		image.setAttribute("src", "../gallery/final_img_" + i + ".png");
-		document.getElementById("list").appendChild(image);
+		document.getElementsByClassName("list").appendChild(image);
 	}
 }
 
