b74886a4756005843f91a62eedb4fddcea94fa96
Manny <Immanuel.Almosara@stonybrook.edu>
2019-09-21 13:00:07 -0400

    no more errors

--- webapp/app/static/scripts/camera.js
+++ webapp/app/static/scripts/camera.js
@@ -1,47 +1,43 @@
 //--------------------
-      // GET USER MEDIA CODE
-      //--------------------
+// GET USER MEDIA CODE
+//--------------------
 
 
-      var video;
-      var webcamStream;
+var video;
+var webcamStream;
 
-      function startWebcam() {
-        if (navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices.getUserMedia({ video: true })
-          .then(function (stream) {
-              video.srcObject = stream;
-          })
-          .catch(function (err0r) {
-            console.log("Something went wrong!");
-          });
+function startWebcam() {
+	if (navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices.getUserMedia({ video: true })
+			.then(function (stream) {
+					video.srcObject = stream;
+					})
+		.catch(function (err0r) {
+				console.log("Something went wrong!");
+				});
+	}
 }
-      }
 
 
-      //---------------------
-      // TAKE A SNAPSHOT CODE
-      //---------------------
-      var canvas, ctx;
+//---------------------
+// TAKE A SNAPSHOT CODE
+//---------------------
+var canvas, ctx;
 
-      function init() {
-        video = document.querySelector('video');
-        // Get the canvas and obtain a context for
-        // drawing in it
-        canvas = document.getElementById("myCanvas");
-        context = canvas.getContext('2d');
-      }
+function init() {
+	video = document.querySelector('video');
+	// Get the canvas and obtain a context for
+	// drawing in it
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext('2d');
+}
 
-      function snapshot() {
-        // Draws current image from the video element into the canvas
-        console.log(webcamStream);
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        webcamStream.getTracks().forEach(function(track) {
-          track.stop();
-        });
-        var dataURL = canvas.toDataURL('image/jpeg', 1.0);
-        document.querySelector('#dl-btn').href = dataURL;
+function snapshot() {
+	// Draws current image from the video element into the canvas
+	context.drawImage(video, 0, 0, canvas.width, canvas.height);
+	var dataURL = canvas.toDataURL('image/jpeg', 1.0);
+	document.querySelector('#dl-btn').href = dataURL;
+	console.log(dataURL)
+	//NOW WE NEED TO SEND IT TO FLASK
 
-        console.log(dataURL)
-
-      }
+}
